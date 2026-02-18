7e64decf272abc73d313a8bc7207d13c8098c56e
Sirasith Supasarn <6810110510@psu.ac.th>
2026-02-18 13:19:04 +0700

    connent_docker wit nest and add README.md

--- our-web/backend/src/app.module.ts
+++ our-web/backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'admin',
+      password: 'password123',
+      database: 'Finalproy1_dev',
+      entities: [], // เพิ่ม Entities ได้ที่นี่ในภายหลัง
+      synchronize: true, // สร้าง Table อัตโนมัติ
+    }),
+  ],
 })
 export class AppModule {}
